6d5232d050824892d7bd073c2e1ebb28263f6cff
0301sdh <128281771+0301sdh@users.noreply.github.com>
2023-04-05 16:49:08 +0900

    Update volume_calc.py
    주석을 달아봤다

--- volume_calc.py
+++ volume_calc.py
@@ -1,7 +1,7 @@
-length = float(input("가로:"))
-width = float(input("세로:"))
-height = float(input("높이:"))
+length = float(input("가로:"))  #가로
+width = float(input("세로:"))   #세로
+height = float(input("높이:"))  #높이
 
-volume = length*width*height
+volume = length*width*height    #가로*세로*높이=부피
 
-print("박스의 부피는", volume, "입니다.")
+print("박스의 부피는", volume, "입니다")
