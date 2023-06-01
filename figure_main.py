37d82ed89c5fe692e1fed0d196e10fd19a4c11c5
0301sdh <0301sdh@naver.com>
2023-06-01 17:32:49 +0900

    implement hw5

---
+++ figure_main.py
@@ -0,0 +1,10 @@
+import figure
+
+myline = figure.line(10, 20)
+
+width, height = myline.get_length()
+try:
+    rectangle = figure.area_rectangle(width, height)
+    print(rectangle)
+except ValueError:
+    print("please input positive number for width and height")
